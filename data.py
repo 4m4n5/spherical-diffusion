cdba1e838b3e4c3cd4714a236d5a148daa4b09a2
Aman Shrivastava <aman.srivastava999@gmail.com>
2023-01-31 19:25:09 -0500

    init run

---
+++ data.py
@@ -0,0 +1,86 @@
+import json
+import os, random
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+import torchvision.transforms as T
+from PIL import Image
+
+
+def get_circle_dot_data(args):
+    train_transforms = torchvision.transforms.Compose([
+        T.Resize(args.img_size),  # args.img_size + 1/4 *args.img_size
+        # T.RandomResizedCrop(args.img_size, scale=(0.8, 1.0)),
+        T.ToTensor(),
+        T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    train_dataset = CircleDotDataset(json_file=args.dataset_path, image_transform=train_transforms)
+
+    return train_dataset
+
+
+class CircleDotDataset(torch.utils.data.Dataset):
+    def __init__(
+        self, 
+        json_file, 
+        image_transform, 
+        percentage=100.0,
+    ):
+        self.ann = json.load(open(json_file, "r"))
+
+        # Prune dataset to percentage
+        random.shuffle(self.ann)
+        if percentage < 100.0:
+            to_remove = int((100.0 - percentage) / 100.0 * len(self.ann))
+            self.ann = self.ann[to_remove:]
+
+        self.image_transform = image_transform
+
+    def __len__(self):
+        return len(self.ann)
+
+    def __getitem__(self, idx):
+        item = self.ann[idx]
+
+        if not os.path.isfile(item["image"]):
+            raise FileNotFoundError(item["image"])
+
+        image = Image.open(item["image"]).convert("RGB")
+        image = self.image_transform(image)
+        
+        angle = item["angle"]
+        clz = item["class"]
+
+        return image, angle, int(clz)
+
+
+def create_sampler(datasets, shuffles, num_tasks, global_rank):
+    samplers = []
+    for dataset,shuffle in zip(datasets,shuffles):
+        sampler = torch.utils.data.DistributedSampler(dataset, num_replicas=num_tasks, rank=global_rank, shuffle=shuffle)
+        samplers.append(sampler)
+    return samplers     
+
+
+def create_loader(datasets, samplers, batch_size, num_workers, is_trains, collate_fns):
+    loaders = []
+    for dataset,sampler,bs,n_worker,is_train,collate_fn in zip(datasets,samplers,batch_size,num_workers,is_trains,collate_fns):
+        if is_train:
+            shuffle = (sampler is None)
+            drop_last = True
+        else:
+            shuffle = False
+            drop_last = False
+        loader = DataLoader(
+            dataset,
+            batch_size=bs,
+            num_workers=n_worker,
+            pin_memory=True,
+            sampler=sampler,
+            shuffle=shuffle,
+            collate_fn=collate_fn,
+            drop_last=drop_last,
+        )              
+        loaders.append(loader)
+    return loaders  
---
+++ ddpm_conditional.py
@@ -0,0 +1,195 @@
+"""
+This code train a conditional diffusion model on CIFAR.
+It is based on @dome272.
+@wandbcode{condition_diffusion}
+"""
+
+import argparse
+from contextlib import nullcontext
+import os
+import copy
+import numpy as np
+from utils import set_seed
+import torch
+from torch import optim
+import torch.nn as nn
+from types import SimpleNamespace
+from fastprogress import progress_bar, master_bar
+import utils
+from utils import *
+from data import get_circle_dot_data, create_loader, create_sampler
+from modules import UNet_conditional, EMA
+import logging
+import wandb
+from diffusion import Diffusion
+import random
+import torch.backends.cudnn as cudnn
+
+
+
+def cycle(dataloader, start_iteration: int = 0):
+    r"""
+    A generator to yield batches of data from dataloader infinitely.
+    Internally, it sets the ``epoch`` for dataloader sampler to shuffle the
+    examples. One may optionally provide the starting iteration to make sure
+    the shuffling seed is different and continues naturally.
+    """
+    iteration = start_iteration
+
+    while True:
+        if isinstance(dataloader.sampler, torch.utils.data.DistributedSampler):
+            # Set the `epoch` of DistributedSampler as current iteration. This
+            # is a way of determinisitic shuffling after every epoch, so it is
+            # just a seed and need not necessarily be the "epoch".
+            dataloader.sampler.set_epoch(iteration)
+
+        for batch in dataloader:
+            yield batch
+            iteration += 1
+
+
+config = SimpleNamespace(    
+    run_name = "DDPM_conditional",
+    noise_steps=1000,
+    seed = 42,
+    batch_size = 8,
+    img_size = 64,
+    num_classes = 1000,
+    dataset_path = r"/localtmp/as3ek/data/dot_circle/images/path_angle.json",
+    device = "cuda",
+    slice_size = 1,
+    use_wandb = True,
+    do_validation = False,
+    fp16 = True,
+    num_workers=2,
+    lr = 1e-4,
+    log_interval = 10,
+    num_steps = 10000,
+)
+
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt="%I:%M:%S")
+
+
+def parse_args(config):
+    parser = argparse.ArgumentParser(description='Process hyper-parameters')
+    parser.add_argument('--run_name', type=str, default=config.run_name, help='name of the run')
+    parser.add_argument('--batch_size', type=int, default=config.batch_size, help='batch size')
+    parser.add_argument('--img_size', type=int, default=config.img_size, help='image size')
+    parser.add_argument('--num_classes', type=int, default=config.num_classes, help='number of classes')
+    parser.add_argument('--dataset_path', type=str, default=config.dataset_path, help='path to dataset')
+    parser.add_argument('--use_wandb', type=bool, default=config.use_wandb, help='use wandb')
+    parser.add_argument('--lr', type=float, default=config.lr, help='learning rate')
+    parser.add_argument('--slice_size', type=int, default=config.slice_size, help='slice size')
+    parser.add_argument('--noise_steps', type=int, default=config.noise_steps, help='noise steps')
+    parser.add_argument('--num_steps', type=int, default=config.num_steps, help='num training steps')
+    parser.add_argument('--log_interval', type=int, default=config.log_interval, help='num training steps')
+    # parser.add_argument('--config', default='./configs/pretrain.yaml')
+    # parser.add_argument('--output_dir', default='output/Pretrain')  
+    # parser.add_argument('--checkpoint', default='')
+
+    # Distributed training parameters    
+    parser.add_argument('--evaluate', action='store_true')    
+    parser.add_argument('--device', default='cuda')
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')    
+    parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--distributed', default=True, type=bool)
+    args = vars(parser.parse_args())
+    
+    # update config with parsed args
+    for k, v in args.items():
+        setattr(config, k, v)
+
+def main(config):
+    utils.init_distributed_mode(config)
+    device = torch.device(config.device)
+
+    # fix the seed for reproducibility
+    seed = config.seed + utils.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    cudnn.benchmark = False
+
+    if utils.is_main_process():
+        wandb.init(project="spherical_diffusion", config=vars(config))
+
+    #### Dataset #### 
+    print("Creating dataset")
+    datasets = [get_circle_dot_data(config)]
+    print('number of training samples: %d'%len(datasets[0]))
+
+    num_tasks = utils.get_world_size()
+    global_rank = utils.get_rank()
+
+    samplers = create_sampler(datasets, [True], num_tasks, global_rank)         
+    data_loader = create_loader(
+        datasets, 
+        samplers, 
+        batch_size=[config.batch_size], num_workers=[config.num_workers], is_trains=[True], collate_fns=[None]
+    )[0]
+
+    start_iteration = 0
+    train_dataloader_iter = cycle(data_loader, start_iteration)
+
+    #### Model ####
+    print("Creating model") 
+    diffuser = Diffusion(config.noise_steps, img_size=config.img_size, num_classes=config.num_classes)
+    ema = EMA(0.995)
+    
+    mk_folders(config.run_name)
+    
+    optimizer = optim.AdamW(diffuser.model.parameters(), lr=config.lr, weight_decay=0.001)
+    mse = nn.MSELoss()
+    scaler = torch.cuda.amp.GradScaler()
+
+    model_without_ddp = diffuser.model
+    if config.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(diffuser.model, device_ids=[config.gpu])
+        model_without_ddp = model.module
+
+    # Start training
+    diffuser.model.train()
+    avg_loss = 0
+    for iteration in range(start_iteration + 1, config.num_steps + 1):
+        optimizer.zero_grad()
+        batch = next(train_dataloader_iter)
+        with torch.autocast("cuda") and torch.enable_grad():
+            images = batch[0].to(device)
+            labels = batch[2].to(device)
+            t = diffuser.sample_timesteps(images.shape[0]).to(device)
+            x_t, noise = diffuser.noise_images(images, t)
+            if np.random.random() < 0.1:
+                labels = None
+            
+            predicted_noise = diffuser.model(x_t, t, labels)
+            loss = mse(noise, predicted_noise)
+            avg_loss += loss
+            
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+            ema.step_ema(diffuser.ema_model, diffuser.model)
+
+            if iteration % config.log_interval == 0:
+                if config.use_wandb:
+                    if utils.is_main_process():
+                        wandb.log({
+                            "loss": loss.item(),
+                            "learning_rate": optimizer.param_groups[0]['lr'], 
+                        })
+                        # diffuser.log_images(use_wandb=config.use_wandb)
+
+            torch.distributed.barrier()
+            
+    diffuser.save_model("first")
+
+
+if __name__ == '__main__':
+    parse_args(config)
+
+    ## seed everything
+    set_seed(config.seed)
+    main(config)
